17ae5089849a3f4f9459eba0dde09bf7b5af3967
Bryan <bryanlohjw97@gmail.com>
2022-08-26 22:57:17 +0800

    build: weatherAPI module
    fetch functions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-async function getCoordinates(location) {
-  try {
-    const latPromise = fetch();
-    const lonPromise = fetch();
-    const [lat, lon] = await Promise.all([latPromise, lonPromise]);
-    return [lat, lon];
-  } catch (error) {
-    console.log(error);
-  }
-}
+import getWeather from './weatherAPI';
 
-async function getWeather([lat, lon]) {
-  try {
-    const key = '4e767ccf870324498e595e40179c0c00';
-    const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
-    const weatherData = await weatherPromise.json();
-    const { weather } = weatherData;
-  } catch (error) {
-    console.log(error);
-  }
-  // console.log(weather);
-  return weather;
-}
+const startUp = function () {
+  const body = document.querySelector('body');
+  const content = document.createElement('div');
+  content.classList.add('content');
+  body.appendChild(content);
+};
 
-const weather = getWeather(['30', '40']);
-
-console.log('hl');
+startUp();
+getWeather('London');
---
+++ src/weatherAPI.js
@@ -0,0 +1,17 @@
+async function getWeather(location) {
+  try {
+    const key = 'cd422b923b03f0e42f9bffddb3a4239d';
+    const geoPromise = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${key}`, { mode: 'cors' });
+    const geoData = await geoPromise.json();
+    const { lon } = geoData[0];
+    const { lat } = geoData[0];
+
+    const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
+    const weatherData = await weatherPromise.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getWeather;

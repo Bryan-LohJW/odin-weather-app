a1359656319669d9bfaf20d1dc6ed5d59ed0b62d
Bryan <bryanlohjw97@gmail.com>
2022-08-26 23:30:15 +0800

    feat: weatherName

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,3 +1,8 @@
+const weatherState = (weatherData) => {
+  const { name } = weatherData;
+  console.log(name);
+};
+
 async function getWeather(location) {
   try {
     const key = 'cd422b923b03f0e42f9bffddb3a4239d';
@@ -9,6 +14,7 @@ async function getWeather(location) {
     const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`);
     const weatherData = await weatherPromise.json();
     console.log(weatherData);
+    weatherState(weatherData);
   } catch (error) {
     console.log(error);
   }
